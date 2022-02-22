6ef77ea16868fba38a0c1e85b99a3e5e78412f86
Lucas Huang <lucas.huang@gentrack.com>
2022-02-22 16:47:23 +1300

    Adjusted for minimal hosting model

--- ExpenseReconciliation/Program.cs
+++ ExpenseReconciliation/Program.cs
@@ -1,26 +1,96 @@
-using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Configuration;
+using ExpenseReconciliation.DataContext;
+using ExpenseReconciliation.Domain.Repositories;
+using ExpenseReconciliation.Domain.Services;
+using ExpenseReconciliation.Repository;
+using ExpenseReconciliation.Services;
+using Microsoft.EntityFrameworkCore;
 
-namespace ExpenseReconciliation
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+// In production, the React files will be served from this directory
+builder.Services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(
+            builder.Configuration.GetConnectionString("defaultConnection"))
+        .UseSnakeCaseNamingConvention()
+        .EnableSensitiveDataLogging() // TODO: Dev only
+);
+
+builder.Services.AddCors(options =>
 {
-    public class Program
+    options.AddPolicy("Dev", builder =>
     {
-        public static void Main(string[] args)
-        {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .Build();
+    });
+});
 
-            CreateHostBuilder(args).Build().Run();
-            
-        }
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IDashboardService, DashboardService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IDashboardRepository, DashboardRepository>();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseSpaStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        "default",
+        "{controller}/{action=Index}/{id?}");
+});
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+
+    if (builder.Environment.IsDevelopment())
+        // spa.UseReactDevelopmentServer("dev");
+        spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+});
+
+app.Run();
+
+// using System;
+// using System.Collections.Generic;
+// using System.Data.SqlClient;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.Extensions.Logging;
+//
+// namespace ExpenseReconciliation
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//
+//             CreateHostBuilder(args).Build().Run();
+//             
+//         }
+//
+//         public static IHostBuilder CreateHostBuilder(string[] args) =>
+//             Host.CreateDefaultBuilder(args)
+//                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+//     }
+// }
--- ExpenseReconciliation/Startup.cs
+++ ExpenseReconciliation/Startup.cs
@@ -1,95 +1,88 @@
-using ExpenseReconciliation.Domain.Repositories;
-using ExpenseReconciliation.Domain.Services;
-using ExpenseReconciliation.Repository;
-using ExpenseReconciliation.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
-namespace ExpenseReconciliation
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-
-            // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
-
-            services.AddDbContext<DataContext.AppDbContext>(options => options.UseNpgsql(
-                Configuration.GetConnectionString("defaultConnection"))
-                .UseSnakeCaseNamingConvention()
-                .EnableSensitiveDataLogging() // TODO: Dev only
-            );
-
-            services.AddCors(options =>
-            {
-                options.AddPolicy("Dev", builder =>
-                {
-                    builder.AllowAnyOrigin()
-                        .AllowAnyHeader()
-                        .AllowAnyMethod()
-                        .Build();
-                });
-            });
-
-            services.AddScoped<IUserService, UserService>();
-            services.AddScoped<IDashboardService, DashboardService>();
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<IDashboardRepository, DashboardRepository>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseCors("Dev");
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseSpaStaticFiles();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    "default",
-                    "{controller}/{action=Index}/{id?}");
-            });
-
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    // spa.UseReactDevelopmentServer("dev");
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
-                }
-            });
-        }
-    }
-}
+// using ExpenseReconciliation.DataContext;
+// using ExpenseReconciliation.Domain.Repositories;
+// using ExpenseReconciliation.Domain.Services;
+// using ExpenseReconciliation.Repository;
+// using ExpenseReconciliation.Services;
+// using Microsoft.EntityFrameworkCore;
+//
+// namespace ExpenseReconciliation
+// {
+//     public class Startup
+//     {
+//         public Startup(IConfiguration configuration)
+//         {
+//             Configuration = configuration;
+//         }
+//
+//         public IConfiguration Configuration { get; }
+//
+//         // This method gets called by the runtime. Use this method to add services to the container.
+//         public void ConfigureServices(IServiceCollection services)
+//         {
+//             services.AddControllersWithViews();
+//
+//             // In production, the React files will be served from this directory
+//             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
+//
+//             services.AddDbContext<AppDbContext>(options => options.UseNpgsql(
+//                         Configuration.GetConnectionString("defaultConnection"))
+//                     .UseSnakeCaseNamingConvention()
+//                     .EnableSensitiveDataLogging() // TODO: Dev only
+//             );
+//
+//             services.AddCors(options =>
+//             {
+//                 options.AddPolicy("Dev", builder =>
+//                 {
+//                     builder.AllowAnyOrigin()
+//                         .AllowAnyHeader()
+//                         .AllowAnyMethod()
+//                         .Build();
+//                 });
+//             });
+//
+//             services.AddScoped<IUserService, UserService>();
+//             services.AddScoped<IDashboardService, DashboardService>();
+//             services.AddScoped<IUserRepository, UserRepository>();
+//             services.AddScoped<IDashboardRepository, DashboardRepository>();
+//         }
+//
+//         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//         {
+//             if (env.IsDevelopment())
+//             {
+//                 app.UseCors("Dev");
+//                 app.UseDeveloperExceptionPage();
+//             }
+//             else
+//             {
+//                 app.UseExceptionHandler("/Error");
+//                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//                 app.UseHsts();
+//             }
+//
+//             app.UseHttpsRedirection();
+//             app.UseStaticFiles();
+//             app.UseSpaStaticFiles();
+//
+//             app.UseRouting();
+//
+//             app.UseEndpoints(endpoints =>
+//             {
+//                 endpoints.MapControllerRoute(
+//                     "default",
+//                     "{controller}/{action=Index}/{id?}");
+//             });
+//
+//             app.UseSpa(spa =>
+//             {
+//                 spa.Options.SourcePath = "ClientApp";
+//
+//                 if (env.IsDevelopment())
+//                     // spa.UseReactDevelopmentServer("dev");
+//                     spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+//             });
+//         }
+//     }
+// }
