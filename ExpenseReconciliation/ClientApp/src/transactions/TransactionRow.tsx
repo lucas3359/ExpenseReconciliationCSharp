940ee28125e0a98f0703a951d2cb5f48fda105c2
Thomas Elliott <thomas-elliott@users.noreply.github.com>
2022-09-11 16:17:08 +1200

    Pagination (#12)
    * Add pagination to the transaction API get calls
    
    * Add pagination to the frontend
    
    * Fix tests that broke due to adding in ordering on get calls
    
    * change name to userName to match the backend api
    change split api to function from NextJS
    
    * small adjustments
    
    * Fix splits username displaying incorrectly
    
    Co-authored-by: Lucas Huang <lucas.huang@gentrack.com>

--- ExpenseReconciliation/ClientApp/src/transactions/TransactionRow.tsx
+++ ExpenseReconciliation/ClientApp/src/transactions/TransactionRow.tsx
@@ -71,6 +71,10 @@ const TransactionRow = ({
   const renderCurrency = (amount: number): string => {
     return (amount / 100).toFixed(2);
   };
+  
+  const renderSplitDetails = (splits: Split[]) => {
+    return splits.map(split => `${users.find(user => user.id === split.userId)?.userName}: ${renderCurrency(split.amount)}`).join(' ')
+  }
 
   return (
     <>
@@ -79,7 +83,7 @@ const TransactionRow = ({
         key={`row-${row.id}`}
       >
         <td className="p-2 text-gray-600">{renderDate(row.date)}</td>
-        <td className="p-2">{row.details}</td>
+        <td className="p-2">{row.details}<br />{renderSplitDetails(row.splits)}</td>
         <td
           className={`p-2 text-right font-semibold ${
             Number(row.amount) < 0 ? 'text-gray-600' : 'text-green-400'
