6a6d67bf48c62d683144cd34ebded5ae11e44cb7
Thomas Elliott <thomaselliott@outlook.com>
2022-11-28 21:38:24 +1300

    Import PrimeNG

--- ExpenseReconciliation/ClientApp/tailwind.config.js
+++ ExpenseReconciliation/ClientApp/tailwind.config.js
@@ -8,16 +8,5 @@ module.exports = {
   },
   plugins: [
     require("@tailwindcss/typography"),
-    require("daisyui"),
   ],
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
-  },
 }
